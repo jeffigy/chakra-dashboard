bbd9d709ec623acf7fc1e385641d56a571b466c8
jeffigy <evangelistajeffy25@gmail.com>
2023-04-20 20:34:57 +0800

    fixed datepicker styling

--- src/components/Form/FormInputs/DatePicker.tsx
+++ src/components/Form/FormInputs/DatePicker.tsx
@@ -1,15 +1,21 @@
 import { ErrorMessage, Field } from "formik";
 import React, { FunctionComponent } from "react";
 import { Input } from "@chakra-ui/react";
-import DateView from "react-datepicker";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import TextError from "./TextError";
+
 type DatePickerProps = {
   label: string;
   name: string;
   [key: string]: any;
 };
 
-const DatePicker: React.FC<DatePickerProps> = ({ label, name, ...rest }) => {
+const DatePickerComponent: React.FC<DatePickerProps> = ({
+  label,
+  name,
+  ...rest
+}) => {
   return (
     <div className="form-control">
       <label htmlFor={name}>{label}</label>
@@ -18,7 +24,8 @@ const DatePicker: React.FC<DatePickerProps> = ({ label, name, ...rest }) => {
           const { setFieldValue } = form;
           const { value } = field;
           return (
-            <DateView
+            <DatePicker
+              className="chakra-input css-1f1trk8"
               id={name}
               {...field}
               {...rest}
@@ -32,4 +39,5 @@ const DatePicker: React.FC<DatePickerProps> = ({ label, name, ...rest }) => {
     </div>
   );
 };
-export default DatePicker;
+
+export default DatePickerComponent;
